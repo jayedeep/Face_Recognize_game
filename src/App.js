01a15964f9898b02b291ad1175ced61f54c0b24d
jayedeep <jaydeeptalaviya8@gmail.com>
2020-08-26 21:41:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import AnimatedImg from "./components/AnimatedImg";
+import AnimatedList from "./components/AnimatedList";
+import Helper from "./components/Helper";
+import NewGameAgain from "./components/NewGameAgain";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <AnimatedImg /> */}
+
+      {/* <AnimatedList />
+      <AnimatedList />
+      <AnimatedList /> */}
+      {/* <Helper /> */}
+      <NewGameAgain />
     </div>
   );
 }
